73f9e58dda89040af961ce42a92864b9c5a37858
Calvin <calvingsx@gmail.com>
2024-07-15 17:06:03 -0300

    Commit inicial

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Login Page",
+  description: "Página de login para testar conhecimentos em componentes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
