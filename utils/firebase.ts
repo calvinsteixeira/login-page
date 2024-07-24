9c27be992e7f30f627fb2d2e82cd40b6f9879063
Calvin <calvingsx@gmail.com>
2024-07-24 15:00:49 -0300

    Ajustando uso de variáveis de ambiente em deploy e local

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,16 +1,29 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const firebaseLocalConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const firebaseDeployConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
 
-export { auth }
+const firebaseConfig = isDevelopment ? firebaseLocalConfig : firebaseDeployConfig
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
