012f9d7661e5b3d60d59c7e35f84e683225f5d1c
Calvin <calvingsx@gmail.com>
2024-07-29 08:00:21 -0300

    Removendo método desnecessário

--- lib/actions/getToken.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers"
-
-export async function getToken() {
-    const authToken = await cookies().get('token')?.value
-    if (!authToken) {
-        const headers = new Headers().set('Authorization', `${authToken}`)
-    }
-}
