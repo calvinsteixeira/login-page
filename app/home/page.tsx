606a63c14159150aa4602f9de681ad81557dd620
Calvin <calvingsx@gmail.com>
2024-07-29 17:35:29 -0300

    Ajustando conteúdo

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -16,42 +16,42 @@ export default function Page() {
 
    return (
       <main className="flex w-full min-h-screen flex-col py-12">
-         <div className="flex flex-col gap-4">
-            <h1 className="text-lg">Olá{userName && <span className={"font-bold"}> {userName}</span>}, você está logado de forma segura!</h1>
-            <p className="text-sm">Alguns dos caprichos utilizados nesse processo:</p>
+         <h1 className="text-base">Você está logado de forma segura...</h1>
+         <div className="flex flex-col gap-4 mt-8">
+            <p className="text-sm"><span className="font-bold">{userName}</span>, esses são alguns dos caprichos utilizados nesse projeto</p>
+            <ul className="flex flex-col gap-4">
+               <li className="flex flex-row gap-3 items-center">
+                  <span>
+                     <FcApproval />
+                  </span>{" "}
+                  Firebase para autenticação
+               </li>
+               <li className="flex flex-row gap-3 items-center">
+                  <span>
+                     <FcApproval />
+                  </span>{" "}
+                  Firestore para dados adicionais do seu cadastro
+               </li>
+               <li className="flex flex-row gap-3 items-center">
+                  <span>
+                     <FcApproval />
+                  </span>{" "}
+                  Metodologia segura de autenticação
+               </li>
+               <li className="flex flex-row gap-3 items-center">
+                  <span>
+                     <FcApproval />
+                  </span>{" "}
+                  Cadastro simplificado utilizando o Google
+               </li>
+               <li className="flex flex-row gap-3 items-center">
+                  <span>
+                     <FcApproval />
+                  </span>{" "}
+                  Responsividade com mobile first
+               </li>
+            </ul>
          </div>
-         <ul className="mt-4 flex flex-col gap-4">
-            <li className="flex flex-row gap-3 items-center">
-               <span>
-                  <FcApproval />
-               </span>{" "}
-               Firebase para autenticação
-            </li>
-            <li className="flex flex-row gap-3 items-center">
-               <span>
-                  <FcApproval />
-               </span>{" "}
-               Firestore para dados adicionais do seu cadastro
-            </li>
-            <li className="flex flex-row gap-3 items-center">
-               <span>
-                  <FcApproval />
-               </span>{" "}
-               Metodologia segura de autenticação
-            </li>
-            <li className="flex flex-row gap-3 items-center">
-               <span>
-                  <FcApproval />
-               </span>{" "}
-               Cadastro simplificado utilizando o Google
-            </li>
-            <li className="flex flex-row gap-3 items-center">
-               <span>
-                  <FcApproval />
-               </span>{" "}
-               Responsividade com mobile first
-            </li>
-         </ul>
       </main>
    );
 }
