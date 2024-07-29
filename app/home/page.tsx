f7e761c9b71d6ad319c7d2bde3c1179a2d9bfb09
Calvin <calvingsx@gmail.com>
2024-07-29 17:03:46 -0300

    Adicionando conteúdo na página home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { FcApproval } from "@/icons";
 
 export default function Page() {
    function getCookie(cookieName: string) {
@@ -14,8 +15,43 @@ export default function Page() {
    const userName = getCookie("userName");
 
    return (
-      <main className="flex w-full min-h-screen flex-col items-center justify-center py-12">
-         <h2>Olá{userName && <span className={"font-bold"}> {userName}</span>}, você está logado!</h2>
+      <main className="flex w-full min-h-screen flex-col py-12">
+         <div className="flex flex-col gap-4">
+            <h1 className="text-lg">Olá{userName && <span className={"font-bold"}> {userName}</span>}, você está logado de forma segura!</h1>
+            <p className="text-sm">Alguns dos caprichos utilizados nesse processo:</p>
+         </div>
+         <ul className="mt-4 flex flex-col gap-4">
+            <li className="flex flex-row gap-3 items-center">
+               <span>
+                  <FcApproval />
+               </span>{" "}
+               Firebase para autenticação
+            </li>
+            <li className="flex flex-row gap-3 items-center">
+               <span>
+                  <FcApproval />
+               </span>{" "}
+               Firestore para dados adicionais do seu cadastro
+            </li>
+            <li className="flex flex-row gap-3 items-center">
+               <span>
+                  <FcApproval />
+               </span>{" "}
+               Metodologia segura de autenticação
+            </li>
+            <li className="flex flex-row gap-3 items-center">
+               <span>
+                  <FcApproval />
+               </span>{" "}
+               Cadastro simplificado utilizando o Google
+            </li>
+            <li className="flex flex-row gap-3 items-center">
+               <span>
+                  <FcApproval />
+               </span>{" "}
+               Responsividade com mobile first
+            </li>
+         </ul>
       </main>
    );
 }
--- icons/index.ts
+++ icons/index.ts
@@ -1,6 +1,7 @@
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
-import { FcGoogle } from "react-icons/fc";
+import { FcGoogle, FcApproval  } from "react-icons/fc";
 
-export { AiOutlineLoading3Quarters, FaEye, FaEyeSlash, FcGoogle, MdOutlineKeyboardArrowRight };
+
+export { AiOutlineLoading3Quarters, FaEye, FaEyeSlash, FcGoogle, MdOutlineKeyboardArrowRight, FcApproval  };
